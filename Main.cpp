ab06b584d109b8a86b9f12826a04eede81fd9d16
sakaki-tohru <18378920@gmail.com>
2020-12-26 16:47:30 +0900

    update main.cpppppp

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,27 @@
 size_t round(size_t now) {
 	if (now == 0) return 1;
 	if (now == 678) return 677;
-	
+
 	return now;
+}
+
+auto readText(const String& filename) {
+	auto reader = TextReader();
+	reader.open(filename);
+	if (!reader) {
+		throw Error(U"Failed to open .txt");
+	}
+	String line;
+	String allText = U"";
+	while (reader.readLine(line)) {
+		allText += U"{}\n"_fmt(line);
+	}
+	return allText;
+}
+
+auto constructFilename(int num) {
+	static const String Title = U"honzuki/N4830BU-";
+	return Title + U"{:0>5}"_fmt(num) + U".txt";
 }
 
 void Main()
@@ -14,61 +33,41 @@ void Main()
 	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
 
 	// 大きさ 60 のフォントを用意
-	const Font font(60);
+	const Font font(20);
 
-	TextReader reader;
-	size_t now = 0;
-	String Title = U"honzuki/N4830BU-";
-	reader.open(Title + U"{:0>5}"_fmt(++now) + U".txt");
-	String line=U"";
+	size_t now = 1;
+	double v = 0.0;
+	// textのy座標
+	auto fontYpos = 60.0;
 
-	if (!reader)
-	{
-		throw Error(U"Failed to open .txt");
-	}
-	Print << U"";Print << U"";
-	if (reader.readLine(line))
-	{
-		Print << U"{}"_fmt(line);
-	}
+	// 最初のテキストを読み込み
+	auto text = readText(constructFilename(now));
 
 	while (System::Update())
 	{
 		// マウスカーソルに追従する半透明の赤い円を描く
 		Circle(Cursor::Pos(), 10).draw(ColorF(1, 0, 0, 0.5));
 
-
-		// [Space] キーが押されたら
-		if (KeySpace.down())
-		{
-			if (reader.readLine(line)) 
-			{
-				Print << U"{}"_fmt(line);
-			}
-		}
+		font(text).draw(10, fontYpos);
+		fontYpos-=v;
 
 		// ボタンが押されたら
 		if (SimpleGUI::Button(U"Next Story", Vec2(600, 20)))
 		{
 			now = round(now + 1);
-			reader.open(Title+U"{:0>5}"_fmt(now)+U".txt");
-			ClearPrint();
-			Print << U"";Print << U"";
-			if (reader.readLine(line))
-			{
-				Print << U"{}"_fmt(line);
-			}
+			text = readText(constructFilename(now));
+			fontYpos = 60.0;
+			//ClearPrint();
 		}
+		// ボタンが押されたら
 		if (SimpleGUI::Button(U"Prev Story", Vec2(10, 20)))
 		{
 			now = round(now - 1);
-			reader.open(Title + U"{:0>5}"_fmt(now) + U".txt");
-			ClearPrint();
-			Print << U"";Print << U"";
-			if (reader.readLine(line))
-			{
-				Print << U"{}"_fmt(line);
-			}
+			text = readText(constructFilename(now));
+			fontYpos = 60.0;
+			//ClearPrint();
 		}
+		SimpleGUI::Slider(U"読む速度", v, 0.0, 10.0, Vec2(270, 20));
+		//スライド
 	}
 }
