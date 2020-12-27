7a15254ca302f997fbb1f94d471f0e320ca98d76
sakaki-tohru <18378920@gmail.com>
2020-12-27 11:33:22 +0900

    update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -30,13 +30,17 @@ auto constructFilename(int num) {
 void Main()
 {
 	// 背景を水色にする
-	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+	Scene::SetBackground(ColorF(0.5, 0.6, 7.0));
 
-	// 大きさ 60 のフォントを用意
+	// 大きさ 20 のフォントを用意
 	const Font font(20);
 
+	//読んでいる話数
 	size_t now = 1;
+
+	//読む速度
 	double v = 0.0;
+
 	// textのy座標
 	auto fontYpos = 60.0;
 
@@ -57,7 +61,7 @@ void Main()
 			now = round(now + 1);
 			text = readText(constructFilename(now));
 			fontYpos = 60.0;
-			//ClearPrint();
+			v = 0.0;
 		}
 		// ボタンが押されたら
 		if (SimpleGUI::Button(U"Prev Story", Vec2(10, 20)))
@@ -65,9 +69,9 @@ void Main()
 			now = round(now - 1);
 			text = readText(constructFilename(now));
 			fontYpos = 60.0;
-			//ClearPrint();
+			v = 0.0;
 		}
-		SimpleGUI::Slider(U"読む速度", v, 0.0, 10.0, Vec2(270, 20));
 		//スライド
+		SimpleGUI::Slider(U"読む速度", v, -10.0, 10.0, Vec2(270, 20));
 	}
 }
